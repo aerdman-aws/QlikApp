805e5e4b6a354f2b26998bd2696649c68df17d9f
aerdman <aerdman@gmail.com>
2015-05-13 21:32:15 -0400

    Adding basic model and controller

---
+++ QlikApp/Controllers/MessagesController.cs
@@ -0,0 +1,35 @@
+﻿using QlikApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace QlikApp.Controllers
+{
+    public class MessagesController : ApiController
+    {
+        private Message[] messages = new Message[] 
+        { 
+            new Message { Id = 1, Body = "Tomato Soup" }, 
+            new Message { Id = 2, Body = "Yo-yo" }, 
+            new Message { Id = 3, Body = "Hammer" } 
+        };
+
+        public IEnumerable<Message> GetAll()
+        {
+            return messages;
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            var message = messages.FirstOrDefault((p) => p.Id == id);
+            if (message == null)
+            {
+                return NotFound();
+            }
+            return Ok(message);
+        }
+    }
+}
